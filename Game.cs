a8a3fb980815c7cc5ac1ec1c89d65b7db5f01f4f
Андрейс Жестянниковс <azhestyannikovs@edu.hse.ru>
2020-07-08 17:19:55 +0300

    version1

--- Game.cs
+++ Game.cs
@@ -12,9 +12,32 @@ namespace TheSnakeGame
 {
     public partial class Game : Form
     {
+        Area area = new Area();
+       
+
         public Game()
         {
             InitializeComponent();
+            InitializeGame();
+            InitializeSnake();
+        }
+
+        private void InitializeGame()
+        {
+
+            this.Height = 600;
+            this.Width = 600;
+
+            this.Controls.Add(area);
+            area.Top = 100;
+            area.Left = 100;
+            //area.Location = new Point(100, 100);
+            
+        }
+         private void InitializeSnake()
+        {
+            PictureBox snake = new PictureBox();
+            Controls.Add(snake);
         }
     }
 }
---
+++ Snake.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheSnakeGame
+{
+    class Snake
+    {
+        List<PictureBox> snakePixels = new List<PictureBox>();
+        public Snake()
+        {
+            InitializeSnake();
+        }
+
+        private void InitializeSnake()
+        {
+            PictureBox snakePixel = new PictureBox();
+            snakePixel.Height = 20;
+            snakePixel.Width = 20;
+            snakePixel.BackColor = Color.OrangeRed;
+            snakePixels.Add(snakePixel);
+        }
+
+        public void Render()
+        {
+
+        }
+    }
+}
